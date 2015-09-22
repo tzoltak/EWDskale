2a7fec3e11d01af8bf4456f80717b57be9b29f9a
tzoltak <t.zoltak@ibe.edu.pl>
2015-09-22 16:05:27 +0200

    Jeszcze poprawka w lacz_kryteria_z_korelacji().

--- R/lacz_kryteria_z_korelacji.R
+++ R/lacz_kryteria_z_korelacji.R
@@ -104,8 +104,7 @@ lacz_kryteria_z_korelacji_w_ramach_czesci_egz = function(x, katalogDane, prog,
   dane = filter_(dane, ~populacja_wy & !pomin_szkole)
   dane = dane[, grep("^[kp]_", names(dane))]
   # radzenie sobie z syfem, jaki przytrafia się w danych
-  maska = ncol(dane) == rowSums(is.na(dane))
-  dane = subset(dane, !get("maska"))
+  dane = subset(dane, ncol(dane) > rowSums(is.na(dane)))
   message("  Wczytano dane z wynikami egzaminu.")
 
   # przygotowanie obiektu tylko z interesującymi nas parami zmiennych
@@ -170,9 +169,6 @@ lacz_kryteria_z_korelacji_w_ramach_czesci_egz = function(x, katalogDane, prog,
     dane[, pary$kryterium[wierszMax]] = rowSums(dane[, c(pary$kryterium[wierszMax],
                                                          pary$kryterium2[wierszMax])])
     pary$korelacja[pary$kryterium %in% k2 | pary$kryterium2 %in% k2] = NA
-    # radzenie sobie z syfem, jaki przytrafia się w danych
-    maska = ncol(dane) == rowSums(is.na(dane))
-    dane = subset(dane, !get("maska"))
     # przeliczanie korelacji
     maska = which(pary$kryterium %in% k1 & !is.na(pary$korelacja))
     maska = maska[maska != wierszMax]
@@ -186,6 +182,8 @@ lacz_kryteria_z_korelacji_w_ramach_czesci_egz = function(x, katalogDane, prog,
     if (sum(maska) <= 1) {
       next
     }
+    # radzenie sobie z syfem, jaki przytrafia się w danych
+    dane = subset(dane, sum(maska) > rowSums(is.na(dane[, maska])))
     model = suppressMessages(mirt(dane[, maska], 1, TOL = 0.01, verbose = FALSE))
     # to jest to samo, ale naklepane z wartościami startowymi - tyle że daje z 8% zysku szybkości
     #pars = mod2values(model)[, c("item", "class", "name", "value")]
